03815869c72d3608cb06fb93eafc55347afdf4f9
Ramon Xavier <ramon1234rxm@gmail.com>
2021-07-09 20:58:34 -0300

    feat: add switch to react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Home from "./pages/Home";
 import NewRoom from "./pages/NewRoom";
@@ -10,9 +10,11 @@ function App() {
   return (
     <BrowserRouter>
       <AuthContextProvider>
-        <Route path="/" exact component={Home} />
-        <Route path="/rooms/new" exact component={NewRoom} />
-        <Route path="/rooms/:id" component={Room} />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+        </Switch>
       </AuthContextProvider>
     </BrowserRouter>
   );
