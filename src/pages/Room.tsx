5c6561430c5cdf094ebf3da537f77b0ab404373f
Ramon Xavier <ramon1234rxm@gmail.com>
2021-07-09 21:57:12 -0300

    style: create a styles for room component

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,17 +1,17 @@
-import logoImg from ".../assets/images/logo.svg";
 import Button from "../components/Button";
 
+import "../styles/room.scss";
 const Room = () => {
   return (
     <div id="page-room">
       <header>
         <div className="content">
-          <img src={logoImg} alt="Letmeask" />
+          <img src="../assets/images/logo.svg" alt="Letmeask" />
           <div>codigo</div>
         </div>
       </header>
 
-      <main className="content">
+      <main>
         <div className="room-title">
           <h1>Sala React</h1>
           <span>4 perguntas</span>
