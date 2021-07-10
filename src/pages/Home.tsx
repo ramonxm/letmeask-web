df50186cb3b8693c29c590dba27549c233635978
Ramon Xavier <ramon1234rxm@gmail.com>
2021-07-09 21:08:49 -0300

    feat: flow to enter a room

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,15 @@
+import { FormEvent } from "react";
+import { useState } from "react";
 import { useHistory } from "react-router-dom";
-
 import { useAuth } from "../hooks/useAuth";
 
 import Button from "../components/Button";
 import "../styles/auth.scss";
+import { database } from "../services/firebase";
 
 const Home = () => {
   const history = useHistory();
+  const [roomCode, setRoomCode] = useState("");
   const { user, signInWithGoogle } = useAuth();
   async function handleCreateRoom() {
     if (!user) {
@@ -14,6 +17,23 @@ const Home = () => {
     }
     history.push("/rooms/new");
   }
+
+  const handleJoinRoom = async (e: FormEvent) => {
+    e.preventDefault();
+
+    if (roomCode.trim() === "") {
+      return;
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if (!roomRef.exists()) {
+      alert("Room does not exists.");
+      return;
+    }
+
+    history.push(`/rooms/${roomCode}`);
+  };
   return (
     <div id="page-auth">
       <aside>
@@ -34,7 +54,12 @@ const Home = () => {
           </button>
           <div className="separator">Ou entre em uma sala</div>
           <form>
-            <input type="text" placeholder="Digite o código da sala" />
+            <input
+              type="text"
+              placeholder="Digite o código da sala"
+              onChange={(e) => setRoomCode(e.target.value)}
+              value={roomCode}
+            />
             <Button type="submit">Entrar na sala</Button>
           </form>
         </div>
