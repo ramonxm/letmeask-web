b4cd7081af07215bcf5081fcc250dc03ac3e5c24
Ramon Xavier <ramon1234rxm@gmail.com>
2021-07-21 21:16:12 -0300

    Create a admin page

---
+++ src/pages/AdminRoom.tsx
@@ -0,0 +1,54 @@
+import { useParams } from "react-router-dom";
+// import { useAuth } from "../hooks/useAuth";
+import Button from "../components/Button";
+import RoomCode from "../components/RoomCode";
+import "../styles/room.scss";
+import Question from "../components/Question";
+import useRoom from "../hooks/useRoom";
+
+type RoomParams = {
+  id: string;
+};
+
+const AdminRoom = () => {
+  // const { user } = useAuth();
+  const params = useParams<RoomParams>();
+  const roomId = params.id;
+
+  const { title, questions } = useRoom(roomId);
+
+  return (
+    <div id="page-room">
+      <header>
+        <div className="content">
+          <img src="../assets/images/logo.svg" alt="Letmeask" />
+          <div>
+            <RoomCode code={roomId} />
+            <Button isOutlined>Encerrar sala</Button>
+          </div>
+        </div>
+      </header>
+
+      <main>
+        <div className="room-title">
+          <h1>Sala {title}</h1>
+          {questions.length > 0 && <span>{questions.length} pergunta(s)</span>}
+        </div>
+
+        <div className="questions-list">
+          {questions.map((question) => {
+            return (
+              <Question
+                key={question.id}
+                content={question.content}
+                author={question.author}
+              />
+            );
+          })}
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default AdminRoom;
