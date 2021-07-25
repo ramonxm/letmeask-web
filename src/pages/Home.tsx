9e755d6980a5d6e499d56b4c30a4455337127ff6
Ramon Xavier <ramon1234rxm@gmail.com>
2021-07-25 14:31:09 -0300

    Alert room end

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -31,6 +31,11 @@ const Home = () => {
       return;
     }
 
+    if (roomRef.val().endedAt) {
+      alert("Room already closed.");
+      return;
+    }
+
     history.push(`/rooms/${roomCode}`);
   };
   return (
