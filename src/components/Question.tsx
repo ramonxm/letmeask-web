b025c5976bb212b038e57f1c6210fe1c770f8b6b
Ramon Xavier <ramon1234rxm@gmail.com>
2021-07-25 16:37:59 -0300

    Update props and add cx class

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
-
+import cx from "classnames";
 import "../styles/question.scss";
 
 type QuestionProps = {
@@ -9,11 +9,25 @@ type QuestionProps = {
     avatar: string;
   };
   children?: ReactNode;
+  isAnswered?: boolean;
+  isHighlighted?: boolean;
 };
 
-const Question = ({ content, author, children }: QuestionProps) => {
+const Question = ({
+  content,
+  author,
+  isAnswered = false,
+  isHighlighted = false,
+  children,
+}: QuestionProps) => {
   return (
-    <div className="question">
+    <div
+      className={cx(
+        "question",
+        { answered: isAnswered },
+        { highlighted: isHighlighted && !isAnswered }
+      )}
+    >
       <p>{content}</p>
       <footer>
         <div className="user-info">
